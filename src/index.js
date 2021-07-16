876ac05059a36a52fceccd46df033bde0ccb8d9f
[Andres] <[hurtadoandres942@gmail.com]>
2021-07-16 13:01:43 -0500

    primmer commit config webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { saludar } from './js/components';
+import './style.css';
+
+
+const nombre = 'Andres';
+saludar( nombre );
---
+++ src/js/components.js
@@ -0,0 +1,13 @@
+import "../css/components.css"
+// import webpackLogo from "../assets/img/webpack-logo.png"
+
+export const saludar = ( nombre ) => {
+    console.log('creando etiqueta h1');
+    const h1 = document.createElement('h1');
+    h1.innerText = `hola, ${ nombre }`;
+    document.body.append(h1);
+
+    // const img = document.createElement('img');
+    // img.src = webpackLogo;
+    // document.body.append(img)
+}
